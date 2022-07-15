5c55a861f6551a9dc5d374057ef38e0d54839dd9
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-15 12:12:23 -0400

    add test structure

---
+++ src/test/java/com/github/rayniel95/velocitest/awsappsync/util/UtilTest.java
@@ -0,0 +1,20 @@
+package com.github.rayniel95.velocitest.awsappsync.util;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class UtilTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
