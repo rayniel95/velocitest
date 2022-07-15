b22c4b702a1be71516995d459a716a8bdb37341a
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-15 12:17:30 -0400

    add template execution

--- src/test/java/com/github/rayniel95/velocitest/awsappsync/util/UtilTest.java
+++ src/test/java/com/github/rayniel95/velocitest/awsappsync/util/UtilTest.java
@@ -3,6 +3,21 @@ package com.github.rayniel95.velocitest.awsappsync.util;
 import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
+import java.io.StringWriter;
+import java.io.File;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+
+import org.apache.velocity.app.VelocityEngine;
+import org.apache.velocity.Template;
+import org.apache.velocity.VelocityContext;
+import org.apache.commons.io.FileUtils;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+import com.google.gson.GsonBuilder;
+import java.lang.reflect.Type;
+import com.github.rayniel95.velocitest.awsappsync.util.*;
+
 
 /**
  * Unit test for simple App.
@@ -16,5 +31,32 @@ public class UtilTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+
+    public static void runTemplate(String templatePath, String contextPath) throws Exception {
+        VelocityEngine engine = new VelocityEngine();
+        engine.init();
+        
+        try {
+            Template template = engine.getTemplate(templatePath);
+            String input = FileUtils.readFileToString(
+                new File(contextPath), StandardCharsets.UTF_8
+            );
+            
+            Gson gson = new GsonBuilder().enableComplexMapKeySerialization().create();
+            Type contextType = new TypeToken<HashMap<String, Object>>(){}.getType();
+            HashMap<String, Object> loadedContext = gson.fromJson(input, contextType);
+            
+            loadedContext.put("util", new Util());
+
+            VelocityContext context = new VelocityContext(loadedContext);
+
+            StringWriter writer = new StringWriter();
+            template.merge(context, writer);
+
+            System.out.println(writer.toString());
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
 }
