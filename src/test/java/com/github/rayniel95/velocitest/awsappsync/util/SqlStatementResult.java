21152076a56304e1eae9f7107dfb73a343a950e9
Rainyel Ramos <rainyel.ramos@gmail.com>
2022-07-18 16:27:28 -0400

    add sqlstatement result data class

--- src/test/java/com/github/rayniel95/velocitest/awsappsync/util/SqlStatementResult.java
+++ src/test/java/com/github/rayniel95/velocitest/awsappsync/util/SqlStatementResult.java
@@ -1,5 +1,10 @@
 package com.github.rayniel95.velocitest.awsappsync.util;
 
+import java.util.List;
+
 public class SqlStatementResult {
-    
+    int numberOfRecordsUpdated;
+    List<Object> records;
+    List<Object> columnMetadata;
 }
+
